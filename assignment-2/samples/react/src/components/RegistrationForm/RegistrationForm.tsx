ab3dcdef09899ba678427e0a8479ef3f7fbf5a15
shusuke919 <sh091965@gmail.com>
2024-12-09 20:42:53 +0900

    form画面UI作成

--- assignment-2/samples/react/src/components/RegistrationForm/RegistrationForm.tsx
+++ assignment-2/samples/react/src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import "./RegistrationForm.css";
+
+type FormData = {
+  name: string;
+  email: string;
+  postalCode: string;
+  prefecture: string;
+  address: string;
+  building?: string;
+};
 
 const RegistrationForm = () => {
-  return (
-    <div>RegistrationForm</div>
-  )
-}
+  const { register } = useForm<FormData>();
 
-export default RegistrationForm
+  return (
+    <form>
+      <div>
+        <label>氏名</label>
+        <input type="text" placeholder="(例) トレタ 太郎" />
+      </div>
+
+      <div>
+        <label>Eメール</label>
+        <input type="email" placeholder="(例)yoyaku@toreta.in" />
+      </div>
+
+      <div>
+        <label>郵便番号</label>
+        <input type="text" placeholder="(例)0000000" />
+      </div>
+
+      <div>
+        <label>都道府県</label>
+        <select>
+          <option value="">選択してください</option>
+          <option value="東京都">東京都</option>
+          <option value="大阪府">大阪府</option>
+        </select>
+      </div>
+
+      <div>
+        <label>市区町村・番地</label>
+        <input type="text" placeholder="(例)品川区西五反田7丁目22-17" />
+      </div>
+
+      <div>
+        <label>建物名・号室</label>
+        <input
+          type="text"
+          placeholder="(例)TOCビル 8F"
+          {...register("building")}
+        />
+      </div>
+
+      <button type="submit">登録</button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
